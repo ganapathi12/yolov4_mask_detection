aa7b52d8dba163b024757d6fbde83ce2b9706208
Balaji D <balajidass07@gmail.com>
2020-09-12 17:59:00 +0530

    Add files via upload

---
+++ camera_to_program.py
@@ -0,0 +1,13 @@
+
+import cv2
+import numpy as np
+url = 'http://192.168.43.71:8080/video'
+cap = cv2.VideoCapture(url)
+while(True):
+    ret, frame = cap.read()
+    if frame is not None:
+        cv2.imshow('frame',frame)
+    q = cv2.waitKey(1)
+    if q == ord("q"):
+        break
+cv2.destroyAllWindows()
